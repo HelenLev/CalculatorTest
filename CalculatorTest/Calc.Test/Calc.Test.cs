627f517ed5cd69a8af6c9a2a4c3bdbf547288d76
HelenLev <95683344+HelenLev@users.noreply.github.com>
2021-12-22 17:20:07 +0200

    Add files via upload

---
+++ CalculatorTest/Calc.Test/Calc.Test.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Calc.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CalculatorTest/ConsoleApp4/additional.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class additional
+    {
+        public static double Sum(double value1, double value2)
+        { 
+            return (value1 + value2); 
+        }
+        public static double Minus(double value1, double value2)
+        {
+            return (value1 - value2);
+        }
+        public static double Multiple(double value1, double value2)
+        {
+            return (value1 * value2);
+        }
+        public static double Division(double value1, double value2)
+        {
+            if (value2 == 0)
+            {
+                throw new DivideByZeroException("You can't division 0");
+            }
+            else
+            {
+                return (value1 / value2);
+            }
+            
+        }
+        public static double Square(double value)
+            
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Value less then 0");
+            }
+            else
+            {
+                return Math.Sqrt(value);
+            }
+            
+        }
+
+    }
+}
